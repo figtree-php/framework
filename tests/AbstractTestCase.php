e65a90d03d440dcd5e803b2e93811bf443ca455f
Alan Ondra <r.alan.ondra@gmail.com>
2021-05-05 15:38:03 -0400

    Strip file reading from tests.

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -3,16 +3,8 @@
 namespace FigTree\Framework\Tests;
 
 use PHPUnit\Framework\TestCase;
-use FigTree\Framework\Tests\Concerns\ReadsTestData;
 
 abstract class AbstractTestCase extends TestCase
 {
-	use ReadsTestData;
-
-	public function __construct()
-	{
-		parent::__construct();
-
-		$this->setDataDir(__DIR__ . '/Data');
-	}
+	//
 }
