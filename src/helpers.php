3c06ba08b87d01ae29d8a097df8d6419b2107f2d
Alan Ondra <r.alan.ondra@gmail.com>
2024-07-18 15:29:22 -0400

    Documentation fix.

--- src/helpers.php
+++ src/helpers.php
@@ -6,7 +6,7 @@ if (!function_exists('is_stringable')) {
 	/**
 	 * Check if the given value is a string or an object which can be cast as a string via strval.
 	 *
-	 * @param [type] $value
+	 * @param mixed $value
 	 *
 	 * @return boolean
 	 */
