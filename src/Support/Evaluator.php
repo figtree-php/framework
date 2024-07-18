ac1fe4997df7d3de239fc204e1c971f8f0dec24a
Alan Ondra <r.alan.ondra@gmail.com>
2024-07-18 15:23:40 -0400

    Increase security for Evaluator.

--- src/Support/Evaluator.php
+++ src/Support/Evaluator.php
@@ -6,8 +6,9 @@ namespace FigTree\Framework\Support;
 
 use Throwable;
 use FigTree\Framework\Core\Context;
-use FigTree\Exceptions\{
+use FigTree\Framework\Exceptions\{
 	InvalidPathException,
+	SecurityException,
 	UnreadablePathException,
 };
 
@@ -34,8 +35,9 @@ class Evaluator
 	 *
 	 * @return mixed
 	 *
-	 * @throws \FigTree\Exceptions\InvalidPathException
-	 * @throws \FigTree\Exceptions\UnreadablePathException
+	 * @throws \FigTree\Framework\Exceptions\InvalidPathException
+	 * @throws \FigTree\Framework\Exceptions\SecurityException
+	 * @throws \FigTree\Framework\Exceptions\UnreadablePathException
 	 */
 	public function read(string $filename, array $data = []): mixed
 	{
@@ -45,17 +47,45 @@ class Evaluator
 			throw new InvalidPathException($path);
 		}
 
+		if (!str_starts_with($path, $this->context->path())) {
+			throw new SecurityException("Access to the file '{$filename}' is not allowed.");
+		}
+
+		if (!file_exists($path) || !is_readable($path)) {
+			throw new UnreadablePathException($filename);
+		}
+
+		$safeData = [];
+
+		foreach ($data as $key => $value) {
+			if (is_string($key) && preg_match('/^[a-zA-Z_][a-zA-Z0-9_]*$/', $key)) {
+				$safeData[$key] = $value;
+			}
+		}
+
+		$loader = new class($path) {
+			public function __construct(private string $path)
+			{
+				//
+			}
+
+			public function __invoke(array $data = []): mixed
+			{
+				extract($data, EXTR_PREFIX_SAME, '_');
+				return (include $this->path) ?? null;
+			}
+		};
+
 		ob_start();
 
 		try {
-			extract($data, EXTR_PREFIX_SAME, '_');
-			$data = (include $path) ?? null;
+			$result = $loader($data);
 		} catch (Throwable $exc) {
 			throw new UnreadablePathException($path, 0, $exc);
 		} finally {
 			ob_end_clean();
 		}
 
-		return $data;
+		return $result;
 	}
 }
